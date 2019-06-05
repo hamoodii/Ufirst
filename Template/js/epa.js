0c5253f1f50f62c055d823001002f7b838ebbe9f
hamoodii <Mohammed.khairi@powercoders.org>
2019-06-05 22:58:34 +0200

    add chart & ajax jsondata

--- Template/js/epa.js
+++ Template/js/epa.js
@@ -1,5 +1,16 @@
 jQuery(document).ready(function() {
     console.log('document.ready');
   // TODO: implementation
+  $.getJSON( "ajax/test.json", function( data ) {
+    const items = [];
+    $.each( data, function( key, val ) {
+      items.push( "<li id='" + key + "'>" + val + "</li>" );
+    });
+
+    $( "<ul/>", {
+      "class": "my-new-list",
+      html: items.join( "" )
+    }).appendTo( "body" );
+  });
 });
 console.log('script.ready');
--- public/js/epa.js
+++ public/js/epa.js
@@ -1,5 +1,38 @@
 jQuery(document).ready(function() {
     console.log('document.ready');
   // TODO: implementation
+  $.getJSON( "ajax/test.json", function( data ) {
+    console.log(data);
+    const labels = [];
+    const items = [];
+    $.each( data, function( key, val ) {
+      labels.push(key);
+      items.push(val);
+    });
+    console.log(items);
+    genChart(items, labels);
+  });
 });
+
+function genChart(data, labels){
+  const ctx = document.getElementById('requestsPerMinuteChart').getContext('2d');
+  const chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: labels,
+        datasets: [{
+            label: 'Requests per minute',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: data
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+  });
+}
 console.log('script.ready');
